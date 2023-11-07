155eed02efdf6ceacce31c84da692dcb84051491
wizock <silent.death3500@gmail.com>
2023-11-08 05:57:06 +1000

    Add client_dev to .gitignore and update createCard class to generate Mastercard numbers

--- src/accounts/acounts.ts
+++ src/accounts/acounts.ts
@@ -1,24 +1,19 @@
-class createCard {
-	cardType: string;
+import { ICard } from "./interfaces";
+import db from "../db";
 
-	constructor(cardType: string) {
-		this.cardType = cardType;
+class createCard {
+	dateOfIssue: Date;
+
+	constructor() {
+		this.dateOfIssue = new Date();
 	}
 
-	generateCreditCardNumber(): string {
-		// const cardType = Math.random() < 0.5 ? "Visa" : "Mastercard";
-		// const cardType = "Mastercard";
-
-		let prefix = "";
-		// if (cardType === "Visa") {
-		prefix = "4";
-		// } else {
-		prefix = (Math.floor(Math.random() * 5) + 51).toString();
-		// }
-		const remainingDigits = Array.from({ length: 15 }, () =>
-			Math.floor(Math.random() * 10),
-		).join("");
-		const cardNumber = prefix + remainingDigits;
+	generateCardNumber(): string {
+		let prefix = "5"; // Start with '5' for Mastercard.
+		for (let i = 0; i < 14; i++) {
+			prefix += Math.floor(Math.random() * 10).toString();
+		}
+		const cardNumber = prefix;
 		return cardNumber;
 	}
 
@@ -34,24 +29,50 @@ class createCard {
 		return (10 - (sum % 10)) % 10;
 	}
 
-	generateCVV() {}
+	generateCVV() {
+		return `${Math.floor(Math.random() * 5)}${Math.floor(
+			Math.random() * 5,
+		)}${Math.floor(Math.random() * 5)}`;
+	}
 
-	generateExpireDate() {}
+	generateExpireDate() {
+		// expiry date is 4 years and 2 months from DOI
+		const expiryDate = new Date(this.dateOfIssue);
+		expiryDate.setFullYear(expiryDate.getFullYear() + 4);
+		expiryDate.setMonth(expiryDate.getMonth() + 2);
+
+		return expiryDate.toLocaleDateString("en-AU");
+	}
+	getDateOfIssue() {
+		return this.dateOfIssue.toLocaleDateString();
+	}
+	getCardNumber() {
+		const cardPrefix = this.generateCardNumber();
+		const cardSuffix = this.calculateLuhnChecksum(cardPrefix);
+		const cardNumber = cardPrefix + cardSuffix;
+		return cardNumber;
+	}
+
+	getCard(): ICard {
+		return {
+			CardNumber: newDebit.getCardNumber(),
+			CVV: newDebit.generateCVV(),
+			CardType: "MasterCard",
+			CardIssuedDate: newDebit.getDateOfIssue(),
+			CardExpiryDate: newDebit.generateExpireDate(),
+		};
+	}
+
+	verifyElements() {
+		// search the database for the elements
+	}
+
+	saveToDatabase() {
+		// we encrypt the card details symmetrically with the user's id as the key
+		// but we provide a physical card identity that's saved on the client side
+	}
 }
 
-/**
- * users fill out a form
- * they give personal information
- * the backend generates seeded data for their virtual card
- * [
- * 	- card number
- *  - card cvv
- *  - card expire date
- * 	] write a create method for information generation
- * [] write a update method for modifying the card
- *
- */
+const newDebit = new createCard();
 
-const newDebit = new createCard("debit");
-
-console.log(newDebit.generateCreditCardNumber());
+console.log(newDebit.getCard());
--- src/accounts/interfaces.ts
+++ src/accounts/interfaces.ts
@@ -2,7 +2,10 @@ export interface ICard {
 	CardNumber: string; //1234 5678 9012 3456
 	CVV: string; // 123
 	CardType: string; // MasterCard
-	CardHolderName: string; // john doe
+	CardIssuedDate: string;
 	CardExpiryDate: string; // 12/29
+}
+export interface ICardUserSpecific extends ICard {
+	CardHolderName: string; // john doe
 	ATMPin: number;
 }
